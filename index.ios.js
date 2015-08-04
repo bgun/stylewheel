44a90bc7d498c3e4405624ea46df187a74a00683
Ben Gundersen <ben@bengundersen.com>
2015-08-03 23:06:43 -0400

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var React = require('react-native');
+var { AppRegistry } = React;
+
+var stylewheel = require('./app/main.js');
+
+AppRegistry.registerComponent('stylewheel', () => stylewheel);
