ce331134c16d68e9252b28f4f8cdae36a4375852
Ben Gundersen <ben@bengundersen.com>
2015-08-13 02:37:58 -0400

    image testing

--- app/main.js
+++ app/main.js
@@ -4,26 +4,60 @@ var { Image, StyleSheet, Text, View } = React;
 
 var Swiper = require('react-native-swiper');
 
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+});
+var outfitStyle = {
+  flex: 1,
+  width: 320,
+  height: 568
+}
+var swiperTopStyle = {
+  alignItems: 'stretch',
+  flex: 1
+};
+var swiperBotStyle = {
+  alignItems: 'stretch',
+  flex: 1
+};
+var imageStyle = {
+  alignItems: 'stretch',
+  flex: 1,
+  width: 520,
+  height: 1000,
+};
+
 module.exports = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Image source={ require('image!layer_1') } style={{ flex: 1 }}>
-          <Swiper height={ 284 }>
-            <View style={{ alignItems: 'stretch', flex: 1 }}>
-              <Image source={ require('image!layer_2') } />
-            </View>
-            <View style={{ alignItems: 'stretch', flex: 1 }}>
-              <Image source={ require('image!layer_3') } />
-            </View>
+        <Image style={ outfitStyle } source={ require('image!outfit1') }>
+          <Swiper height={ 400 }>
+            <View style={ swiperTopStyle }><Image style={ imageStyle } source={ require('image!scarves1') } /></View>
+            <View style={ swiperTopStyle }><Image style={ imageStyle } source={ require('image!scarves2') } /></View>
+            <View style={ swiperTopStyle }><Image style={ imageStyle } source={ require('image!scarves3') } /></View>
+            <View style={ swiperTopStyle }><Image style={ imageStyle } source={ require('image!scarves4') } /></View>
+            <View style={ swiperTopStyle }><Image style={ imageStyle } source={ require('image!scarves5') } /></View>
+            <View style={ swiperTopStyle }><Image style={ imageStyle } source={ require('image!scarves6') } /></View>
+            <View style={ swiperTopStyle }><Image style={ imageStyle } source={ require('image!scarves7') } /></View>
+            <View style={ swiperTopStyle }><Image style={ imageStyle } source={ require('image!scarves8') } /></View>
+            <View style={ swiperTopStyle }><Image style={ imageStyle } source={ require('image!scarves9') } /></View>
           </Swiper>
-          <Swiper height={ 284 }>
-            <View style={{ alignItems: 'stretch', flex: 1 }}>
-              <Image source={ require('image!layer_3') } style={{ flex: 1 }} />
-            </View>
-            <View style={{ alignItems: 'stretch', flex: 1 }}>
-              <Image source={ require('image!layer_2') } style={{ flex: 1 }} />
-            </View>
+          <Swiper style={{ height: 400, top: -200 }}>
+            <View style={ swiperBotStyle }><Image style={ imageStyle } source={ require('image!purses1') } /></View>
+            <View style={ swiperBotStyle }><Image style={ imageStyle } source={ require('image!purses2') } /></View>
+            <View style={ swiperBotStyle }><Image style={ imageStyle } source={ require('image!purses3') } /></View>
+            <View style={ swiperBotStyle }><Image style={ imageStyle } source={ require('image!purses4') } /></View>
+            <View style={ swiperBotStyle }><Image style={ imageStyle } source={ require('image!purses5') } /></View>
+            <View style={ swiperBotStyle }><Image style={ imageStyle } source={ require('image!purses6') } /></View>
+            <View style={ swiperBotStyle }><Image style={ imageStyle } source={ require('image!purses7') } /></View>
+            <View style={ swiperBotStyle }><Image style={ imageStyle } source={ require('image!purses8') } /></View>
+            <View style={ swiperBotStyle }><Image style={ imageStyle } source={ require('image!purses9') } /></View>
           </Swiper>
         </Image>
       </View>
@@ -31,11 +65,3 @@ module.exports = React.createClass({
   }
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
