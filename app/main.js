f5b2cc856e6de27680c36fd8328fc92ae1ef6b70
Ben Gundersen <ben@bengundersen.com>
2015-08-26 01:59:12 -0400

    got basic functions working

--- app/main.js
+++ app/main.js
@@ -1,51 +1,54 @@
 var React = require('react-native');
 
-var { Image, StyleSheet, Text, View } = React;
+var { ActionSheetIOS, Image, StyleSheet, Text, TouchableHighlight, View } = React;
 
 var Swiper = require('react-native-swiper');
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1
+  appContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    left: -80
+  },
+  appContainerWithMenu: {
+    flex: 1,
+    flexDirection: 'row',
+    left: 0
+  },
+  outfitContainer: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  menu: {
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    width: 80
   },
   outfitStyle: {
+    alignItems: 'stretch',
     flex: 1,
-    height: 580,
-    position: 'absolute',
-      top: 0,
+    justifyContent: 'center',
     width: 320
   },
   swiperTopStyle: {
-    borderColor: '#FF0000',
-    borderWidth: 5,
-    height: 240,
-    position: 'absolute',
-      left: 50,
-    width: 320
+    alignItems: 'flex-start',
+    borderWidth: 5
   },
   swiperBotStyle: {
-    borderColor: '#0000FF',
-    borderWidth: 10,
-    flex: 1,
-    height: 80,
-    position: 'absolute',
-      top: 0,
-      left: 0,
-    width: 320
+    alignItems: 'flex-start',
+    height: 200
   },
-  scarfStyle: {
-    alignItems: 'stretch',
+  scarfViewStyle: {
+    alignItems: 'flex-start',
     flex: 1
   },
-  purseStyle: {
-    alignItems: 'stretch',
+  purseViewStyle: {
+    alignItems: 'flex-start',
     flex: 1
   },
   scarfImageStyle: {
     alignItems: 'stretch',
     flex: 1,
     width: 320,
-    height: 200,
     top: 285,
     left: 2
   },
@@ -53,16 +56,58 @@ var styles = StyleSheet.create({
     alignItems: 'stretch',
     flex: 1,
     width: 320,
-    height: 200,
     top: -295
+  },
+  numberStyle: {
+    fontSize: 20,
+    height: 20,
+    position: 'absolute',
+      bottom: 200,
+      right: 0,
+    textAlign: 'center',
+    width: 40
   }
 });
+
+var images = {
+  outfit1 : require('image!outfit1'),
+  outfit2 : require('image!outfit2'),
+  outfit3 : require('image!outfit3'),
+  outfit4 : require('image!outfit4'),
+  outfit5 : require('image!outfit5'),
+  outfit6 : require('image!outfit6'),
+  outfit7 : require('image!outfit7'),
+  outfit8 : require('image!outfit8'),
+  outfit9 : require('image!outfit9'),
+  outfit10: require('image!outfit10'),
+  // scarves
+  scarf1: require('image!scarves1'),
+  scarf2: require('image!scarves2'),
+  scarf3: require('image!scarves3'),
+  scarf4: require('image!scarves4'),
+  scarf5: require('image!scarves5'),
+  scarf6: require('image!scarves6'),
+  scarf7: require('image!scarves7'),
+  scarf8: require('image!scarves8'),
+  scarf9: require('image!scarves9'),
+  // purses
+  purse1: require('image!purses1'),
+  purse2: require('image!purses2'),
+  purse3: require('image!purses3'),
+  purse4: require('image!purses4'),
+  purse5: require('image!purses5'),
+  purse6: require('image!purses6'),
+  purse7: require('image!purses7'),
+  purse8: require('image!purses8'),
+  purse9: require('image!purses9'),
+}
 
 var ScarfView = React.createClass({
   render: function() {
     return (
-      <View style={ styles.scarfStyle }>
-        <Image style={ styles.scarfImageStyle } source={ require('image!scarves'+this.props.index) } />
+      <View style={ styles.scarfViewStyle }>
+        <Text style={ styles.numberStyle }>{ this.props.index }</Text>
+        <Image style={ styles.scarfImageStyle } source={ images['scarf'+this.props.index] } />
       </View>
     );
   }
@@ -71,28 +116,85 @@ var ScarfView = React.createClass({
 var PurseView = React.createClass({
   render: function() {
     return (
-      <View style={ styles.purseStyle }>
-        <Image style={ styles.purseImageStyle } source={ require('image!purses'+this.props.index) } />
+      <View style={ styles.purseViewStyle }>
+        <Text style={ styles.numberStyle }>{ this.props.index }</Text>
+        <Image style={ styles.purseImageStyle } source={ images['purse'+this.props.index] } />
       </View>
     );
   }
 });
 
+var OUTFITS = [
+  'pink dress 1',
+  'pink blazer',
+  'pink dress 2',
+  'black and gold',
+  'black dress',
+  'white and black stripes',
+  'cape cod',
+  'green',
+  'leopard',
+  'white'
+];
+
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      outfit: 1,
+      scarf: 0,
+      showMenu: false
+    }
+  },
+  scrolled: function(e, state, context) {
+    this.setState({
+      scarf: context.state.index
+    });
+  },
+  toggleMenu: function() {
+    this.setState({
+      showMenu: !this.state.showMenu
+    });
+  },
+  chooseOutfit: function() {
+    console.log("click");
+    this.setState({
+      showMenu: true
+    });
+    ActionSheetIOS.showActionSheetWithOptions({
+      options: OUTFITS,
+      destructiveButtonIndex: this.state.outfit-1 // used for highlighting!
+    },
+    (buttonIndex) => {
+      this.setState({ outfit: buttonIndex+1 });
+    });
+  },
+
   render: function() {
     return (
-      <View style={styles.container}>
-        <Image style={ styles.outfitStyle } source={ require('image!outfit1') } />
-        <Swiper style={ styles.swiperTopStyle }>
-          { [1,2,3,4,5,6,7,8,9].map(function(i) {
-            <ScarfView index={ i } />
-          }) }
-        </Swiper>
-        <Swiper style={ styles.swiperBotStyle }>
-          { [1,2,3,4,5,6,7,8,9].map(function(i) {
-            <PurseView index={ i } />
-          }) }
-        </Swiper>
+      <View style={ this.state.showMenu ? styles.appContainerWithMenu : styles.appContainer }>
+        <View style={ styles.menu }>
+          <Text>Hello Menu!</Text>
+        </View>
+        <View style={styles.outfitContainer}>
+          <Image style={ styles.outfitStyle } source={ images['outfit'+(this.state.outfit)]}>
+            <Swiper contentContainerStyle={ styles.swiperTopStyle } onMomentumScrollEnd={ this.scrolled.bind(this) }>
+              { [1,2,3,4,5,6,7,8,9].map(function(i) {
+                return <ScarfView key={ i } index={ i } />;
+              }) }
+            </Swiper>
+            <Swiper contentContainerStyle={ styles.swiperBotStyle }>
+              { [1,2,3,4,5,6,7,8,9].map(function(i) {
+                return <PurseView key={ i } index={ i } />;
+              }) }
+            </Swiper>
+          </Image>
+          <TouchableHighlight onPress={ this.toggleMenu } style={{ backgroundColor: '#00FF00', position: 'absolute', left: 10, top: 10, width: 50, height: 50 }}>
+            <Text>hiii</Text>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={ this.chooseOutfit }>
+            <Text style={{ flex: 1, backgroundColor: '#FF0000', height: 30 }}>choose an outfit</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
